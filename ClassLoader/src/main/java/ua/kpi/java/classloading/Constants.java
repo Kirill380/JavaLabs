d4d341fa2d32950f9b7eff7c725248c2aa6d3c1e
Kirill Liubun <kreol_123@ukr.net>
2016-09-19 11:27:11 +0300

    Fix with paths

---
+++ ClassLoader/src/main/java/ua/kpi/java/classloading/Constants.java
@@ -0,0 +1,12 @@
+package ua.kpi.java.classloading;
+
+
+import com.google.common.base.Joiner;
+
+import java.io.File;
+
+public final class Constants {
+    public static final String MODULE_NAME = "ClassLoader";
+    public static final String SRC = Joiner.on(File.separator).join(MODULE_NAME, "src", "main", "java");
+    public static final String TARGET = Joiner.on(File.separator).join(MODULE_NAME, "target", "classes");
+}
